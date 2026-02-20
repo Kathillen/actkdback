f944358832127b22fb5362f2be34c044478b714a
Kathillen <devkathillen@gmail.com>
2026-02-19 23:29:03 -0300

    fix error DB

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 console.log("ENV CHECK:",{
 host: process.env.DB_HOST,
-user: process.env.D_USER,
+user: process.env.DB_USER,
 password: process.env.DB_PASSWORD ? "OK" : "MISSING",
 database: process.env.DB_NAME,
 port: process.env.DB_PORT,
@@ -13,7 +13,7 @@ port: process.env.DB_PORT,
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  user: process.env.D_USER,
+  user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: Number(process.env.DB_PORT),
